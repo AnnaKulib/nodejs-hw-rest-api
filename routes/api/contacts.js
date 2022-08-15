08dbfcbba4e7be162d36ea8e8792882e4ddb1963
Anna Kulibabina <anna.kulibabina@gmail.com>
2022-08-15 12:52:10 +0300

    corrected

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,23 +1,14 @@
 const express = require("express");
-
 const router = express.Router();
-
 const ctrl = require("../../controllers/contacts");
-
 const { ctrlWrapper } = require("../../helpers");
-
 const { validationPost, validationPut } = require("../../middlewares");
-
 const schemas = require("../../schemas/contact");
 
 router.get("/", ctrlWrapper(ctrl.getAll));
-
 router.get("/:id", ctrlWrapper(ctrl.getById));
-
 router.post("/", validationPost(schemas.add), ctrlWrapper(ctrl.add));
-
 router.put("/:id", validationPut(schemas.add), ctrlWrapper(ctrl.updateById));
-
 router.delete("/:id", ctrlWrapper(ctrl.removeById));
 
 module.exports = router;

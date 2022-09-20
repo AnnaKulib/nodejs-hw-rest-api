425666ea9247017775f50ad67c231202f873079c
Anna Kulibabina <anna.kulibabina@gmail.com>
2022-09-20 21:51:09 +0300

    removed console.log

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -2,7 +2,6 @@ const { Contact } = require("../../models");
 
 const add = async (req, res, next) => {
   const { _id: owner } = req.user;
-  console.log(owner);
   const result = await Contact.create({ ...req.body, owner });
 
   if (result) {
--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -4,7 +4,6 @@ const getAll = async (req, res, next) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
-  console.log(req.query);
   const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
     skip,
     limit: Number(limit),
